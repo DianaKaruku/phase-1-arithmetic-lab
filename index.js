b582d33db20a4c010b32a78099f6d27a96fdb24e
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-10-08 19:14:22 +0300

    complete assignment

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ const num4 =10;
 const mod = num3 % num4;
 
 const max = Math.max(4,8,16,20,8);
+function multiplyAndCheck(num1 ,num2) {
+    const result=multiplyAndCheck(num1 ,num2);
+    return result;
+};
+function generateRandomInteger() {
+    return random;
+};
+const generatedRandom = generateRandomInteger();
+const isIntegerGreaterThanZero = Number.isInteger(generatedRandom) && generatedRandom > 0;
+
+function calculateMod(num3, num4) {
+    return num3 % num4;
+  }
+  function findMax(...numbers) {
+    return Math.max(...numbers);
+  }
